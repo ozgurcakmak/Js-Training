d6d5a396dc6b629e4a2e8dbfbac0c3d11efe429a
Ozgur Ozan Cakmak <ozgurcakmak@gmail.com>
2022-10-20 11:41:48 +0300

    roll dice implemented

--- 07-Pig-Game/script.js
+++ 07-Pig-Game/script.js
@@ -4,8 +4,35 @@
 const score0El = document.querySelector('#score--0');
 const score1El = document.getElementById('score--1'); // Getting directly by ID
 const diceEl = document.querySelector('.dice');
+const current0El = document.getElementById('current--0');
+const current1El = document.getElementById('current--1');
+
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
 
 // Resetting scores & hiding dice
 score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
+
+// Scores
+let currentScore = 0;
+
+// Rolling the dice
+btnRoll.addEventListener('click', function () {
+  // 1. Generate a random dice roll
+  const dice = Math.trunc(Math.random() * 6) + 1;
+
+  // 2. Display the dice
+  diceEl.classList.remove('hidden');
+  diceEl.src = `dice-${dice}.png`;
+
+  // 3. Check for rolled 1
+  if (dice !== 1) {
+    // Add dice to the current score
+    currentScore += dice;
+    current0El.textContent = currentScore; // CHANGE LATER!
+  } else {
+  }
+});
